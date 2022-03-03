ba5bcb9f50493703d0b213c2c648ed9db043d0b0
yi-seul <rim4291@naver.com>
2022-03-03 20:58:44 +0900

    Modify server port number

--- src/store/movie.js
+++ src/store/movie.js
@@ -72,7 +72,7 @@ export default {
             })
           }
         }
-      } catch (message) {
+      } catch ({ message }) {
         commit('updateState', {
           movies: [],
           message
@@ -111,23 +111,6 @@ export default {
   }
 }
 
-function _fetchMovie(payload) {
-  const { title, type, year, page, id} = payload
-  const OMDB_API_KEY = '7035c60c'
-  const url = id
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-  return new Promise((resolve, reject) => {
-    axios.get(url)
-      .then(res => {
-        if (res.data.Error) {
-          reject(res.data.Error)
-        }
-        resolve(res)
-      })
-      .catch((err) => {
-        reject(err.message)
-      })
-  })
+async function _fetchMovie(payload) {
+  return await axios.post('/.netlify/functions/movie', payload)
 }
